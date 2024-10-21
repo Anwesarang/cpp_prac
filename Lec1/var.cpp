baebcf49708cf670c1b155e590ab6ee858073a4a
Anwesarang <myselfanwesha33@gmail.com>
2024-10-22 00:05:42 +0530

    lec1

--- Lec1/var.cpp
+++ Lec1/var.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
+
     char grade = 'a';
     int val = grade;
     cout << val <<endl;
+
+    int age;
+    cout << "Enter the age : ";
+    cin >> age;
+
+    cout << "Your age is : " << age << endl;
     return 0;
 }
